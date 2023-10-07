0ca991cf65628c8a7029decbdbb5deba514ea11c
Hudwesley <85462812+hudwesley@users.noreply.github.com>
2023-10-06 22:20:02 -0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,22 @@
+<?php
+$host = "10.90.24.56";
+$port = "5432";
+$user = "aula";
+$password = "aula";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port", $user, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+    $sql = "SELECT * FROM sua_tabela"; 
+    $stmt = $pdo->query($sql);
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "ID: " . $row['id'] . ", Nome: " . $row['nome'] . "<br>";
+    }
+} catch (PDOException $e) {
+    echo "Erro na conexão ou consulta: " . $e->getMessage();
+}
+?>
